561b42faa5f6398431e184298bfb948271f64db7
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-11-24 10:40:40 +0200

    init project

---
+++ src/main/java/ivan/nppsystem/NppSystemApplication.java
@@ -0,0 +1,13 @@
+package ivan.nppsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NppSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NppSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ivan/nppsystem/NppSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ivan.nppsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NppSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
